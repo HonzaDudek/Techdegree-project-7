91ce649c744b070bfbc3619c1f75e7f1f95cefdc
HonzaDudek <honza@honzadudek.cz>
2018-08-11 15:55:11 +0200

    Final

--- js/app.js
+++ js/app.js
@@ -2,16 +2,14 @@ const startScreen = document.getElementById("overlay");
 const startButton = document.querySelector(".btn__reset");
 const guessedPhrase = document.getElementById("phrase").children[0];
 const keyboard = document.getElementById("qwerty");
-const keyboardRow = document.getElementsByClassName("keyrow");
-const buttons = keyboardRow.children;
-let tries = 0;
+
 let missed = document.getElementById("scoreboard");
 let hearts = missed.children[0];
 let missedCount = 0;
 
 // Starting the game by pressing startButton
 startButton.addEventListener("click", () => {
-    startScreen.style.display = "none";
+	startScreen.style.display = "none";
 });
 
 // List of phrases to be generated
@@ -19,23 +17,22 @@ const phrases = ["Hello World", "What a beautiful day", "Keep on coding"];
 
 // Function that generates random array from array, that is passed as a parameter
 
-function getRandomPhrase(array) {
+const getRandomPhrase = (array) => {
     // Pick random phrase from selected array
     const randomPhrase = array[Math.floor(Math.random() * array.length)];
     // convert array to string
     randomPhrase.toString();
-    // split creaed string to letters
-    const randomPhraseArray = randomPhrase.split("");
+    // split created string to letters
+    randomPhrase.split("");
     // Returns single letters from generated array as new array
-    return randomPhraseArray;
-}
+    return randomPhrase;
+};
 
 // Saving generated array to global constant
-const randomPhrase = getRandomPhrase(phrases);
-console.log(randomPhrase);
+let randomPhrase = getRandomPhrase(phrases);
 
 // Creating function, that adds letters to guess to the gameboard
-function addPhraseToDisplay(array) {
+const addPhraseToDisplay = (array) => {
     // Looping through array of letters
     for (let i = 0; i < randomPhrase.length; i += 1) {
         // create <li> element
@@ -43,7 +40,7 @@ function addPhraseToDisplay(array) {
         // give created <li> element textContent from current index of array
         letters.textContent = randomPhrase[i];
         // If the array element is not an empty string, assign class "Letters" to the created <li> element
-        if (letters.textContent != " ") {
+        if (letters.textContent !== " ") {
             letters.className = "letter";
         }
         // If array element is only space, assign class "space" to created <li> element
@@ -53,68 +50,131 @@ function addPhraseToDisplay(array) {
         // append <li> element with its class to guessedPhrase unordered list
         guessedPhrase.appendChild(letters);
     }
-}
+};
 
 // Calling addPhraseToDisplay function - displaying single letters on gameboard
 addPhraseToDisplay(randomPhrase);
 
 // Creating variable letters to store all elements from the <li> element with letters
-let letters = document.querySelectorAll(".letter");
+const letters = document.querySelectorAll(".letter");
+let letterFound = null;
+
 
 const checkLetter = (button) => {
-    let letterFound = null;
-    // Looking for first match of the letters from array with pressed button
-    for (let i = 0; i < letters.length; i += 1)
-        if (letters[i].textContent.toLocaleLowerCase() === button) {
-            // Adds class "Show" to all the letters that match the selection
-            letters[i].classList.add("show");
-            let letterFound = letters[i].textContent;
-            console.log(letterFound);
-            return letterFound;
-        }
-    return letterFound;
+	let buttonText = button.textContent;
 
+	// Looking for first match of the letters from array with pressed button
+    for (let i = 0; i < letters.length; i += 1)
+	if (letters[i].textContent.toLocaleLowerCase() === buttonText) {
+		// Adds class "Show" to all the letters that match the selection
+            letters[i].classList.add("show");
+            letterFound = letters[i].textContent;
+            // console.log(letterFound);
+        }
+        return letterFound;
 };
 
-// let guessedLetter = document.querySelectorAll(".show");
-// let guessedLetters = guessedLetter.length;
-// console.log(guessedLetters);
-// console.log(letters.length);
+// Creating function, what compares number of letters from randomPhrase with number of letters player guessed right
 
+const checkWin = () => {
+	// Saving guessed letters by player to variable guessedLetters
+	let guessedLetters = document.querySelectorAll(".show");
+	// If the player guessed all the letters - change display and class of Start screen
+	if (letters.length === guessedLetters.length) {
+		startScreen.style.display = "flex";
+		startScreen.className = "win";
+		guessedPhrase.style.display = "none";
+		resetFunction();
+	}
+	// If the player lost all his lives - change display and class of Start screen
+	else if (missedCount === 5) {
+		startScreen.style.display = "flex";
+		startScreen.className = "lose";
+		guessedPhrase.style.display = "none";
+		resetFunction();
+	}
+};
 
 keyboard.addEventListener("click", (e) => {
-    let target = e.target;
-    if (e.target.tagName === "BUTTON") {
-        const target = e.target;
-        // Selected letter from keyboard is disabled and class "chosen is added"
-        target.setAttribute("disabled", true);
-        target.className = "chosen";
-    }
-    let button = target.textContent;
-    // Start function to check whether the letter player chose is right
-        checkLetter(button);
-    console.log(checkLetter(button));
-
-    if (checkLetter(button) != null) {
-        console.log("You have guessed it");
+    const button = e.target;
+    if (button.tagName === "BUTTON") {
+        // Selected letter from keyboard is disabled and class "chosen" is added
+        button.setAttribute("disabled", true);
+        button.className = "chosen";
+		// Start function to check whether the letter player chose is right
+		if (checkLetter(button) !== null) {
+		}
+		else {
+			// adds 1 to missed counter and removes last heart
+			missedCount = missedCount + 1;
+			hearts.lastElementChild.remove();
+		}
+		checkWin();
     }
     else {
-        missedCount = missedCount + 1;
-        hearts.lastElementChild.remove();
-        console.log(missedCount);
-    }
-    // checkWin(guessedLetters, missedCount, letters);
-
-
+	}
+    console.log(button);
+	console.log(checkLetter(button));
 });
 
+let resetButton = document.createElement("button");
 
-function checkWin(guessedLetters, missedCount, letters) {
-    if (letters.length = guessedLetters + 1) {
-        startScreen.className = "win";
-    }
-    else if (missedCount === "5") {
-        startScreen.className = "lose";
-    }
-}
+const resetFunction = () => {
+	// creates Reset button
+	// Adding class and text to Reset button
+	resetButton.className = "btn__reset";
+	resetButton.textContent = "Reset game";
+	// appending reset button to win/lose screen
+	startScreen.appendChild(resetButton);
+};
+
+	resetButton.addEventListener("click", (e) => {
+		// Hides win/lose screen on click
+		startScreen.style.display = "none";
+
+		// Counts how many hearts were lost and adds missing back to the scoreboard
+		for (let i = 0; i < missedCount; i += 1) {
+			let heart = document.createElement("li");
+			heart.className = "tries";
+			heart.innerHTML = "<img src='images/liveHeart.png' height='35px' width='30px'>";
+			hearts.appendChild(heart);
+		}
+
+		// resets missedCount to 0
+		missedCount = 0;
+
+		// Saving all chosen letters to a variable resetPicks
+		let resetPicks = document.querySelectorAll(".chosen");
+		// for each chosen letter => remove disabled attribute and class "chosen"
+		for (let i = 0; i < resetPicks.length; i += 1) {
+			resetPicks[i].removeAttribute("disabled");
+			resetPicks[i].className = "";
+		}
+
+		// Saving all letters from previous phrase to a variable resetLetters
+		let resetLetters = document.querySelectorAll(".letter");
+
+		// for each  letter => remove element from DOM
+		for (let i = 0; i < resetLetters.length; i +=1){
+			resetLetters.className = "";
+			guessedPhrase.removeChild(resetLetters[i]);
+		}
+
+		let resetSpaces = document.querySelectorAll(".space");
+		for (let i = 0; i < resetSpaces.length; i +=1){
+			resetSpaces[i].className = "";
+			guessedPhrase.removeChild(resetSpaces[i]);
+		}
+
+		// Generating new random phrase
+		randomPhrase = getRandomPhrase(phrases);
+		addPhraseToDisplay(randomPhrase);
+
+		// Displaying new random phrase
+		guessedPhrase.style.display = "block";
+	});
+
+
+
+
 
